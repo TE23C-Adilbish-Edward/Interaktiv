6fe4a2edd057c2f38eca4e82ab9161c5af54c87a
Edward Adilbish <edward.adilbishvilen@elev.ga.ntig.se>
2024-08-30 14:45:56 +0200

    mjhmjf

---
+++ interaktiv/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Användarnamn:");
+string name = Console.ReadLine();
+
+if (name == "Edward")
+{
+    Console.WriteLine("Välkommen Edward!");
+}
+
+else {
+    Console.WriteLine("Stick härifrån!");
+}
+
+Console.ReadLine();
